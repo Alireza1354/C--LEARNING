186cc397e5295cd730b5d9512f3f1ba7f3736ba7
Karimi <karimifromlfco@gmail.com>
2018-08-31 23:21:45 +0430

    Add Car Model

--- MyApplication/Program.cs
+++ MyApplication/Program.cs
@@ -3,91 +3,90 @@ using System.Linq;
 
 namespace MyApplication
 {
-	internal static class Program
-	{
-		static Program()
-		{
-		}
+    internal static class Program
+    {
+        static Program()
+        {
+        }
 
-		[System.STAThread]
-		internal static void Main()
-		{
-			// **************************************************
-			System.Windows.Forms.Application.EnableVisualStyles();
-			System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-			// **************************************************
+        [System.STAThread]
+        internal static void Main()
+        {
+            // **************************************************
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            // **************************************************
 
-			// **************************************************
-			Models.DatabaseContext databaseContext = null;
+            // **************************************************
+            Models.DatabaseContext databaseContext = null;
 
-			try
-			{
-				databaseContext =
-					new Models.DatabaseContext();
+            try
+            {
+                databaseContext =
+                    new Models.DatabaseContext();
 
-				//int adminCount =
-				//	databaseContext.Users
-				//	.Where(current => current.IsAdmin)
-				//	.Count();
+                //int adminCount =
+                //	databaseContext.Users
+                //	.Where(current => current.IsAdmin)
+                //	.Count();
 
-				//int userCount =
-				//	databaseContext.Users
-				//	.Count();
+                //int userCount =
+                //	databaseContext.Users
+                //	.Count();
 
-				bool hasAnyUser =
-					databaseContext.Users
-					.Any();
+                bool hasAnyUser =
+                    databaseContext.Users
+                    .Any();
 
-				if (hasAnyUser == false)
-				{
-					Models.User adminUser = new Models.User();
+                if (hasAnyUser == false)
+                {
+                    Models.User adminUser = new Models.User
+                    {
+                        IsAdmin = true,
+                        IsActive = true,
+                        Username = "Dariush",
+                        Password = "1234512345",
+                        FullName = "Mr. Dariush Tasdighi"
+                    };
+                    databaseContext.Users.Add(adminUser);
+                    databaseContext.SaveChanges();
+                }
 
-					adminUser.IsAdmin = true;
-					adminUser.IsActive = true;
-
-					adminUser.Username = "Dariush";
-					adminUser.Password = "1234512345";
-					adminUser.FullName = "Mr. Dariush Tasdighi";
-
-					databaseContext.Users.Add(adminUser);
-
-					databaseContext.SaveChanges();
-				}
-                
-			}
-			catch (System.Exception ex)
-			{
-				System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
                 string ms = ex.Message.ToString();
-				return;
-			}
-			finally
-			{
-				if (databaseContext != null)
-				{
-					databaseContext.Dispose();
-					databaseContext = null;
-				}
-			}
+                return;
+            }
+            finally
+            {
+                if (databaseContext != null)
+                {
+                    databaseContext.Dispose();
+                    databaseContext = null;
+                }
+            }
 
             try
             {
                 databaseContext = new Models.DatabaseContext();
-                bool hasAnyBook = databaseContext.Books.Any();
-                if (hasAnyBook==false)
+                bool hasAnyBook =
+                    databaseContext.Books
+                    .Any();
+
+                if (hasAnyBook == false)
                 {
-                    Models.Book myBook = new Models.Book();
-
-                    myBook.AuthorName = "charsDikens";
-                    myBook.ISBN = "kdfjksjflk";
-                    myBook.PublicationDate =DateTime.Now;
-                    myBook.Title = "Beuti";
-
+                    Models.Book myBook = new Models.Book
+                    {
+                        AuthorName = "charsDikens",
+                        ISBN = "kdfjksjflk",
+                        PublicationDate = DateTime.Now,
+                        Title = "Beuti"
+                    };
                     databaseContext.Books.Add(myBook);
                     databaseContext.SaveChanges();
                 }
-
-
             }
             catch (System.Exception ex)
             {
@@ -96,40 +95,75 @@ namespace MyApplication
             }
             finally
             {
-                if(databaseContext!=null)
+                if (databaseContext != null)
                 {
                     databaseContext.Dispose();
                     databaseContext = null;
                 }
             }
-          
 
-			// **************************************************
+            try
+            {
+                databaseContext = new Models.DatabaseContext();
+                bool hasAnyCar =
+                    databaseContext.Cars
+                    .Any();
+                if (hasAnyCar == false)
+                {
+                    Models.Car car = new Models.Car
+                    {
+                        VIN = "B2",
+                        Make = "Toyota",
+                        Model = "4Runner",
+                        StickerPrice = 35000,
+                        Year = 2010
+                    };
 
-			// **************************************************
-			//#region Solution (1)
-			//Infrastructure.BaseForm startupForm = new LoginForm();
-			////Infrastructure.BaseForm startupForm = new RegisterForm();
+                    databaseContext.Cars.Add(car);
+                    databaseContext.SaveChanges();
+                }
 
-			//System.Windows.Forms.Application.Run(startupForm);
 
-			//if (startupForm != null)
-			//{
-			//	if (startupForm.IsDisposed == false)
-			//	{
-			//		startupForm.Dispose();
-			//	}
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString());
+                return;
+            }
+            finally
+            {
+                if(databaseContext !=null)
+                {
+                    databaseContext.Dispose();
+                    databaseContext = null;
+                }
+            }
+            // **************************************************
 
-			//	startupForm = null;
-			//}
-			//#endregion /Solution (1)
-			// **************************************************
+            // **************************************************
+            //#region Solution (1)
+            //Infrastructure.BaseForm startupForm = new LoginForm();
+            ////Infrastructure.BaseForm startupForm = new RegisterForm();
 
-			// **************************************************
-			#region Solution (2)
-			System.Windows.Forms.Application.Run(Infrastructure.Utility.LoginForm);
-			#endregion /Solution (2)
-			// **************************************************
-		}
-	}
+            //System.Windows.Forms.Application.Run(startupForm);
+
+            //if (startupForm != null)
+            //{
+            //	if (startupForm.IsDisposed == false)
+            //	{
+            //		startupForm.Dispose();
+            //	}
+
+            //	startupForm = null;
+            //}
+            //#endregion /Solution (1)
+            // **************************************************
+
+            // **************************************************
+            #region Solution (2)
+            System.Windows.Forms.Application.Run(Infrastructure.Utility.LoginForm);
+            #endregion /Solution (2)
+            // **************************************************
+        }
+    }
 }
